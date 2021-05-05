45feb0fd4ed9961deb9758378b13fea732ce61c7
Luciano Colabraro <luciano.colabraro@gmail.com>
2021-05-05 16:05:14 +0200

    add login, working on post form and pasw encryption

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,21 @@
 // User model here
+
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/login", (req, res, next) => res.render("signup.hbs"));
+
+// router.post("/login"),(req, res, next) => {
+
+// }
+
+// router.post("/login", (req, res, next) => {
+//   const userInfo = req.body;
+//   User.create({ username, password })
+//     .then((user) => {
+//       console.log(user);
+//       // 💥 la url del redirect es completa no como los paths del middleware
+//       res.render("index");
+//     })
+//     .catch((error) => {
+//       next(error);
+//     });
+// });
 
 module.exports = router;
